578ac1188bdbbccb4d685aa2d21c398deb96e71e
andysellick <andysellick@builtvisible.com>
2017-01-16 12:36:35 +0000

    Removing jquery dependence

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 Weather
 =======
 
-A plugin to add weather effects to a webpage. Note: this plugin is unfinished and experimental. I wouldn't recommend using it for anything important yet.
+A plugin to add a rain effect to an element. Options can be passed to control rain appearance. Multiple instances of the plugin on the same page with different configurations are supported.
+
+Note: this plugin no longer has any jQuery dependence.
 
 Usage
 -----
 
+Include the script and do the following.
+
 ```html
-<div class="w-wrapper" id="weather">
+<div id="weather">
     <img src="background.jpg"/>
 </div>
 
 <script>
-    $(document).ready(function(){
-        $('#weather').weather();
-    });
+	var options = {
+		speed: 4,
+		dropDensity: 10,
+		wind: 0
+	};
+	new weatherObj(document.getElementById('weather'),options);
 </script>
 ```
 
-Note that either the height of the element must be set in CSS or determined by an image inside it. CSS is unfinalised.
+Note that either the height of the parent element must be set in CSS or determined by the content inside it.
 
 
 Options
@@ -28,7 +35,8 @@ Options
 - **dropDarkness**: general darkness of drops, is an rgba value, can be anything from 0 to 1 (although 0 would be invisible), defaults to 0.5
 - **speed**: speed of drop fall, defaults to 15
 - **wind**: causes the drops to fall at an angle. 0 for no wind (falls straight) or accepts any positive or negative number, up to a sensible amount, anything beyond 20 just flies across the screen. Generally a higher wind speed looks better with a higher speed. Defaults to -5.
-- **dropwidth**: defaults to 1, not really worth changing
-- **dropheight**: defaults to 2, not really worth changing (drop size is varied automatically to create a depth illusion)
+- **dropWidth**: defaults to 1, not really worth changing
+- **dropHeight**: defaults to 2, not really worth changing (drop size is varied automatically to create a depth illusion)
+- **dropColor**: colour of drops, should be rgb array, defaults to [0,0,0] i.e. black drops
 
 Demo here: http://www.custarddoughnuts.co.uk/article/2015/7/20/plugin-month-weather
--- jquery.weather.js
+++ jquery.weather.js
@@ -1,155 +1,144 @@
-/*
-    latest changes
-    - now allows multiple instances on one page
-    - drop density adjusted
-*/
-(function (window,$) {
-	var Plugin = function(elem,options){
-		this.elem = elem;
-		this.$elem = $(elem);
-		this.options = options
-	}
 
-	Plugin.prototype = {
-		init: function(){
-			var thisobj = this;
-            this.canvas;
-            this.context;
-            this.spacing;
-            this.minstart;
-            this.maxstop;
-            this.drops;
-            this.dropDarkness = [];
+function weatherObj(element,options){
+	this.el = element;
+	this.canvas;
+	this.context;
+	this.spacing;
+	this.minstart;
+	this.maxstop;
+	this.drops;
+	this.dropDarkness = [];
+	this.resize;
+	this.settings = {
+		dropDensity: (typeof options.dropDensity === 'undefined') ? 40 : options.dropDensity, //density of drops
+		dropColor: (typeof options.dropColor === 'undefined') ? [0,0,0] : options.dropColor, //density of drops
+		speed: (typeof options.speed === 'undefined') ? 10 : options.speed,
+		wind: (typeof options.wind === 'undefined') ? -5 : options.wind, //generally a higher wind speed looks better with a higher speed
+		dropDarkness: (typeof options.dropDarkness === 'undefined') ? 0.5 : options.dropDarkness,
+		dropWidth: (typeof options.dropWidth === 'undefined') ? 1 : options.dropWidth,
+		dropHeight: (typeof options.dropHeight === 'undefined') ? 2 : options.dropHeight
+	};
+	this.functions = {
+		canvas: {
+			init: function(){
+				ths.functions.general.overrideSettings();
+				ths.functions.canvas.initCanvas();
+				ths.functions.general.initDrops();
+				ths.functions.general.drawloop();
+			},
+			initCanvas: function(){ // create canvas and get context
+				ths.canvas = document.createElement('canvas');
+				ths.canvas.className = 'w-canvas';
+				//set styles, no need for a stylesheet when they're this simple
+				ths.canvas.style.position = 'absolute';
+				ths.canvas.style.top = 0;
+				ths.canvas.style.left = 0;
+				ths.el.style.position = 'relative'; //parent element
 
-			this.settings = $.extend({
-				weatherType: "rain",
-				dropDensity: 40, //density of drops
-				speed: 10,
-				wind: -5, //generally a higher wind speed looks better with a higher speed
-				dropDarkness: 0.5,
-				dropwidth: 1,
-				dropheight: 2
-			}, this.defaults, this.options);
+				ths.el.appendChild(ths.canvas);
+				ths.context = ths.canvas.getContext('2d');
+				ths.functions.canvas.initialise();
+			},
+			initialise: function(){ //make canvas the same size as its parent, call on window resize
+				ths.canvas.width = ths.el.offsetWidth;
+				ths.canvas.height = ths.el.offsetHeight;
+			},
+			clearCanvas: function(){
+				ths.context.clearRect(0, 0, ths.canvas.width, ths.canvas.height);//clear the canvas
+			}
+		},
+		general: {
+			overrideSettings: function(){ //prevent settings abuse
+				if(ths.settings.wind > 35)
+					ths.settings.wind = 35;
+				if(ths.settings.wind < -35)
+					ths.settings.wind = -35;
+				if(ths.settings.dropDarkness > 1 || ths.settings.dropDarkness < 0)
+					ths.settings.dropDarkness = 1;
+				if(ths.settings.dropColor.constructor !== Array || ths.settings.dropColor.length !== 3)
+					ths.settings.dropColor = [0,0,0];
+			},
+			initDrops: function(){ //create array to store position of drops and initialise related variables
+				//init drop density
+				var dropdensity = (ths.canvas.width / 100) * ths.settings.dropDensity;
+				// set start and end point for falling drops
+				ths.minstart = -20;
+				ths.maxstop = ths.canvas.height + 20;
+				ths.spacing = ths.canvas.width / dropdensity;
+				//init array of drops
+				ths.drops = [];
+				for(var i = 0; i < dropdensity; i++){
+					ths.drops.push([ths.spacing * i,ths.functions.general.randomIntFromInterval(ths.minstart,ths.maxstop)]);
+				}
+				//console.log(thisobj.drops.length,"number of drops");
+				
+				//initialise drop darkness - FIXME
+				ths.dropDarkness.push(ths.settings.dropDarkness);
+				ths.dropDarkness.push((ths.settings.dropDarkness / 3) * 2);
+				ths.dropDarkness.push(ths.settings.dropDarkness / 3);
+			},
+			//return a random number
+			randomIntFromInterval: function(min,max){
+				return Math.floor(Math.random()*(max-min+1)+min);
+			},
+			resizeWindow: function(){
+				ths.functions.canvas.initialise();
+				ths.functions.general.initDrops();
+			},
+			drawloop: function(){ //self calling function that animates the falling precipitation
+				ths.functions.canvas.clearCanvas();
+				var rand = 0.2; //controls the variation in size and speed of the drops. Taller (ie. closer) drops fall quicker and are bigger, giving the illusion of depth. In theory.
+				var dd = 0; //switches between the darkness settings for the drops
+			
+				for(var i = 0; i < ths.drops.length; i++){
+					ths.drops[i][1] = ths.drops[i][1] + ths.settings.speed + (rand * 10); //speed of fall
+					if(ths.drops[i][1] > ths.maxstop){ //reset to top
+						ths.drops[i][1] = ths.minstart;
+					}
 
-			var functions = {
-                canvas: {
-                    initCanvas: function(){ // create canvas and get context
-                        var canvasname = 'canvas' + Date.now(); //generate a unique canvas name, needed for multiple instances
-                        var $canvas = $('<canvas/>').addClass('w-canvas').attr('id',canvasname);
-                        $canvas.appendTo(thisobj.$elem);
-                        thisobj.canvas = document.getElementById(canvasname);
-                        thisobj.context = thisobj.canvas.getContext('2d');
-                        functions.canvas.initialise();
-                    },
-                    initialise: function(){ //make canvas the same size as its parent, call on window resize
-                        thisobj.canvas.width = thisobj.$elem.outerWidth();
-                        thisobj.canvas.height = thisobj.$elem.outerHeight();
-                    },
-                    clearCanvas: function(){
-                        thisobj.context.clearRect(0, 0, thisobj.canvas.width, thisobj.canvas.height);//clear the canvas
-                    }
-                },
-                general: {
-                    overrideSettings: function(){ //prevent settings abuse
-            			if(thisobj.settings.wind > 35)
-                            thisobj.settings.wind = 35;
-            			if(thisobj.settings.wind < -35)
-                            thisobj.settings.wind = -35;
-                        if(thisobj.settings.dropDarkness > 1 || thisobj.settings.dropDarkness < 0)
-                            thisobj.settings.dropDarkness = 1;
-                    },
-                    initDrops: function(){ //create array to store position of drops and initialise related variables
-                        //init drop density
-                        var dropdensity = (thisobj.canvas.width / 100) * thisobj.settings.dropDensity;
-                        // set start and end point for falling drops
-                        thisobj.minstart = -20;
-                        thisobj.maxstop = thisobj.canvas.height + 20;
-                        thisobj.spacing = thisobj.canvas.width / dropdensity;
-                        //init array of drops
-                        thisobj.drops = [];
-                        for(var i = 0; i < dropdensity; i++){
-                            thisobj.drops.push([thisobj.spacing * i,functions.general.randomIntFromInterval(thisobj.minstart,thisobj.maxstop)]);
-                        }
-                        //console.log(thisobj.drops.length,"number of drops");
-                        
-                        //initialise drop darkness - FIXME
-                        thisobj.dropDarkness.push(thisobj.settings.dropDarkness);
-                        thisobj.dropDarkness.push((thisobj.settings.dropDarkness / 3) * 2);
-                        thisobj.dropDarkness.push(thisobj.settings.dropDarkness / 3);
-                    },
-                    //return a random number
-                    randomIntFromInterval: function(min,max){
-                        return Math.floor(Math.random()*(max-min+1)+min);
-                    },
-                    resizeWindow: function(){
-                        functions.canvas.initialise();
-                        functions.general.initDrops();
-                    },
-                    drawloop: function(){ //self calling function that animates the falling precipitation
-                        functions.canvas.clearCanvas();
-                        var rand = 0.2; //controls the variation in size and speed of the drops. Taller (ie. closer) drops fall quicker and are bigger, giving the illusion of depth. In theory.
-                        var dd = 0; //switches between the darkness settings for the drops
-
-                        for(var i = 0; i < thisobj.drops.length; i++){
-                            thisobj.drops[i][1] = thisobj.drops[i][1] + thisobj.settings.speed + (rand * 10); //speed of fall
-                            if(thisobj.drops[i][1] > thisobj.maxstop){ //reset to top
-                                thisobj.drops[i][1] = thisobj.minstart;
-                            }
-
-                            thisobj.context.save();
-                            thisobj.context.fillStyle = 'rgba(0,0,0,' + thisobj.dropDarkness[dd] + ')';//thisobj.settings.colour;
-                            //console.log(thisobj.dropDarkness[dd]);
-                            /* do slightly more complex stuff to translate and rotate drops if wind present */
-                            if(thisobj.settings.wind){
-                                thisobj.drops[i][0] += (thisobj.settings.wind / 2);
-                                if(thisobj.drops[i][0] > thisobj.canvas.width)
-                                    thisobj.drops[i][0] = 0;
-                                if(thisobj.drops[i][0] < 0)
-                                    thisobj.drops[i][0] = thisobj.canvas.width;
-        
-                                thisobj.context.translate(thisobj.drops[i][0], thisobj.drops[i][1]);
-                                thisobj.context.rotate(-thisobj.settings.wind * Math.PI / 90);
-                                //we draw this point at 0,0 because the context has already been translated to the position where we want to draw the drop
-                                thisobj.context.fillRect(0,0, thisobj.settings.dropwidth, thisobj.settings.dropheight + (rand * 8)); //size of drop
-                            }
-                            else {
-                                thisobj.context.fillRect(thisobj.drops[i][0],thisobj.drops[i][1], thisobj.settings.dropwidth, thisobj.settings.dropheight + (rand * 8));
-                            }
-                            thisobj.context.restore();
-        
-                            rand += 0.2;
-                            if(rand > 1){
-                                rand = 0.2;
-                            }
-                            dd += 1;
-                            if(dd >= thisobj.dropDarkness.length){
-                                dd = 0;
-                            }
-                        }
-                        gameloop = setTimeout(functions.general.drawloop,40); //repeat
-                    }
-                }
-            }
-
-            $(window).on('load',function(){
-                functions.general.overrideSettings();
-                functions.canvas.initCanvas();
-                functions.general.initDrops();
-                functions.general.drawloop();
-            });
-
-            var resize;
-        	$(window).on('resize',function(){
-                //don't resize immediately
-                clearTimeout(resize);
-                resize = setTimeout(functions.general.resizeWindow,200);
-        	});
+					ths.context.save();
+					ths.context.fillStyle = 'rgba(' + ths.settings.dropColor[0] + ',' + ths.settings.dropColor[1] + ',' + ths.settings.dropColor[2] + ',' + ths.dropDarkness[dd] + ')';//ths.settings.colour;
+					//console.log(ths.dropDarkness[dd]);
+					/* do slightly more complex stuff to translate and rotate drops if wind present */
+					if(ths.settings.wind){
+						ths.drops[i][0] += (ths.settings.wind / 2);
+						if(ths.drops[i][0] > ths.canvas.width)
+							ths.drops[i][0] = 0;
+						if(ths.drops[i][0] < 0)
+							ths.drops[i][0] = ths.canvas.width;
+			
+						ths.context.translate(ths.drops[i][0], ths.drops[i][1]);
+						ths.context.rotate(-ths.settings.wind * Math.PI / 90);
+						//we draw this point at 0,0 because the context has already been translated to the position where we want to draw the drop
+						ths.context.fillRect(0,0, ths.settings.dropWidth, ths.settings.dropHeight + (rand * 8)); //size of drop
+					}
+					else {
+						ths.context.fillRect(ths.drops[i][0],ths.drops[i][1], ths.settings.dropWidth, ths.settings.dropHeight + (rand * 8));
+					}
+					ths.context.restore();
+			
+					rand += 0.2;
+					if(rand > 1){
+						rand = 0.2;
+					}
+					dd += 1;
+					if(dd >= ths.dropDarkness.length){
+						dd = 0;
+					}
+				}
+				setTimeout(ths.functions.general.drawloop,40); //repeat
+			}
 		}
-	}
-	$.fn.weather = function(options){
-		return this.each(function(){
-			new Plugin(this,options).init();
-		});
-	}
-	window.Plugin = Plugin;
-})(window,jQuery);
+	};
+	var ths = this;
+
+	window.addEventListener('load', function() {
+		ths.functions.canvas.init();
+	},false);
+
+	window.addEventListener('resize', function() {
+		//don't resize immediately
+		clearTimeout(ths.resize);
+		ths.resize = setTimeout(ths.functions.general.resizeWindow,200);
+	},false);
+}
